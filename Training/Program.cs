7e02f46f2548096258b5f6694599aa6f339c4bb6
paulrwd <paulrwd85@gmail.com>
2021-02-13 23:15:10 +0300

    new rep

---
+++ Training/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tLn = Console.ReadLine();
+            int t = int.Parse(tLn);
+            for(int i = 1; i < t; i++)
+            {
+                if(i%3 == 0 && i%5 != 0)
+                {
+                    Console.WriteLine("fuzz");
+                }
+                else if(i%5 == 0 && i%3 != 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else if(i%3 == 0 && i%5 == 0)
+                {
+                    Console.WriteLine("fuzzbuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+            //
+        }
+    }
+}
