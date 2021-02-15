50e1ffda7348261053252509e53daed81115cb61
paulrwd85@gmail.com <paulrwd85@gmail.com>
2021-02-15 12:18:05 +0300

    Update Program.cs

--- Training/Program.cs
+++ Training/Program.cs
@@ -8,7 +8,9 @@ namespace Training
         {
             //BuzzFuzz.Run();
 
-            int res = Factorial.Calculate(5);                   
+            //int res = Factorial.Calculate(5);    
+            
+
         }
 
         
